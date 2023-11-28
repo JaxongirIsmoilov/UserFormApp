b967991c6a60777c312ef9dc7d76eef885fd1a48
Xushnudbek <86800818+XushnudbekTursunboyev@users.noreply.github.com>
2023-11-28 16:49:14 +0500

    draft item

--- app/src/main/java/uz/gita/jaxongir/userformapp/ui/components/DraftItem.kt
+++ app/src/main/java/uz/gita/jaxongir/userformapp/ui/components/DraftItem.kt
@@ -2,9 +2,14 @@ package uz.gita.jaxongir.userformapp.ui.components
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,36 +18,75 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import uz.gita.jaxongir.userformapp.R
-import uz.gita.jaxongir.userformapp.data.local.room.entity.ComponentEntity
 import uz.gita.jaxongir.userformapp.data.model.ComponentData
 
 @Composable
 fun DraftItem(
     componentData: ComponentData,
-    onClick : () -> Unit
+    onClick : (ComponentData) -> Unit
 ) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
+            .height(84.dp)
             .padding(12.dp)
             .border(2.dp, Color.Gray, RoundedCornerShape(10.dp))
             .clickable {
-                onClick()
+                onClick(componentData)
             }
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(start = 16.dp, top = 16.dp, end = 16.dp),
     ) {
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "Draft Form")
-        Spacer(modifier = Modifier.weight(1f))
-        Icon(
-            painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24),
-            contentDescription = "arrow"
-        )
+        Row(modifier = Modifier.fillMaxSize()) {
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Draft FormID:",
+                modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(bottom = 16.dp),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "List:Size", modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(bottom = 16.dp),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(text = "now", modifier = Modifier
+                .align(Alignment.Bottom)
+                .padding(bottom = 2.dp),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold
+                ))
+        }
+
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DraftItemPreview(){
+    //DraftItem(onClick = {})
 }
