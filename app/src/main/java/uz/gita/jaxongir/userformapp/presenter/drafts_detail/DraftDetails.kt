8908315c43a45a5993f7469f6921c2c5dd06d1e4
Xushnudbek <86800818+XushnudbekTursunboyev@users.noreply.github.com>
2023-11-28 15:12:11 +0500

    draft details screen

--- app/src/main/java/uz/gita/jaxongir/userformapp/presenter/drafts_detail/DraftDetails.kt
+++ app/src/main/java/uz/gita/jaxongir/userformapp/presenter/drafts_detail/DraftDetails.kt
@@ -1,12 +1,87 @@
 package uz.gita.jaxongir.userformapp.presenter.drafts_detail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.androidx.AndroidScreen
 import uz.gita.jaxongir.userformapp.data.model.ComponentData
 
 class DraftDetails(private val componentData: ComponentData) : AndroidScreen() {
     @Composable
     override fun Content() {
-        
+        DraftDetailsContent()
     }
+}
+
+@Composable
+fun DraftDetailsContent() {
+    Box(modifier = Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color(0xFFFF3951))
+                    .height(70.dp)
+            ) {
+                Text(
+                    text = "User: Name",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontStyle = FontStyle.Normal,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 16.dp),
+                    color = Color.White
+                )
+
+                Text(
+                    text = "Drafts",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontStyle = FontStyle.Normal,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 16.dp),
+                    color = Color.White
+                )
+
+            }
+
+            LazyColumn(modifier = Modifier.padding(top = 16.dp)){
+
+
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DraftDetailsPreview(){
+    DraftDetailsContent()
 }
